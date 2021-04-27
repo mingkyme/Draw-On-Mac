bc40a7702859e2f2f4281285bce016a2b0823e74
MINGKYME <admin@mingky.me>
2021-04-27 23:41:48 +0900

    Initial Commit

---
+++ Draw-On-Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Draw-On-Mac
+//
+//  Created by MINGKYME on 2021/04/27.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Draw-On-Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Draw-On-Mac
+//
+//  Created by MINGKYME on 2021/04/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
